92dc9c0a1c64152c1ba80e2407239ed7b79d5273
AradiaG <reparacionesteknoles@gmail.com>
2025-06-01 19:23:46 -0600

    01junv8.0

--- js/main.js
+++ js/main.js
@@ -43,7 +43,6 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('visible');
-            observer.unobserve(entry.target); // Solo una vez
         }
     });
 }, {
